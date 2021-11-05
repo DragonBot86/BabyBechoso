f24050161bdae7fef22ce623df39eb2459c6748a
thechoute <82709682+thechoute@users.noreply.github.com>
2021-11-04 22:55:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,18 +8,6 @@ echo -e '\e[1;32m
 ╰━━━┻╯╰┻━━┻━╮╭┻━━━┻━━┻━━┻╯╰┻━━┻━━┻━━╯  ╰╯ ╰╯╰┻━━┻━━━┻╯╰┻━━┻━━┻━┻━━╯
           ╭━╯┃
           ╰━━╯'
-npm install
-npm install -g npm@6
-npm install toml
-npm i crypto-js
-npm i moment-timezone
-npm i axios
-npm i node-fetch@2.6.1 - npm
-npm i requests 
-npm install request --save
-npm i cfonts
-npm i spinnies
-npm i image-to-base64
-npm i remove.bg
-npm i fluent-ffmpeg
 npm i yt-search
+npm i google-it
+npm i g-i-s
